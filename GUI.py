9b1dd9bb5f5d621587c6eedc503a8345c69af11e
TuanQuan-Dev <tuanquan@gmail.com>
2024-06-21 20:54:34 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from wordcloud import WordCloud
 
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -56,8 +57,143 @@ def DNA():
     selected_menu = streamlit_multi_menu(menu_titles=list(sub_menus.keys()), sub_menus=sub_menus,
                             use_container_width=True)
 
-   
+    if selected_menu != None:        
+        if (selected_menu.lower() == "xem"):
+            # <li style="font-size:1.3em; font-weight:bold">Tổng quan dữ liệu:
+            #     <ul>
+            #         Tổng sản phẩm: <b>{myData.Product.shape[0]}</b>         
+            #         Tổng sản phẩm: <b>{myData.Product.shape[0]}</b>
+            #     </ul>
+            # </li>            
+
+            st.write(f"""
+                <div style="font-size:1.1em">
+                 <ol>
+                    <li style="font-size:1.3em; font-weight:bold">Tổng quan dữ liệu:
+                        <ul>
+                            <span>Thời gian phân tích từ ngày <b>01/01/2014</b> đến <b>30/12/2015</b> </span><br/>
+                            <span>Tổng sản phẩm: <b>{myData.Product.shape[0]}</b></span>, tất cả các sản phẩm đều có giao dịch <br/>
+                            <span>Tổng số khách hàng có mua hàng: <b>{myData.Transaction["member_number"].nunique()}</b></span>                           
+                        </ul>
+                    </li>                     
+                </ol>
+                </div> 
+                """
+                 , unsafe_allow_html=True)
+    
+        elif (selected_menu.lower() == "khách hàng"):
+            st.write("""<div style="font-size:1.3em; font-weight:bold">1. Số lượng khách hàng mua hàng theo tháng</div>""", unsafe_allow_html=True)
             
+            df = myData.Transaction
+                        
+            df["month"] = df["order_date"].apply(lambda x: x.strftime("%Y-%m-01"))
+            df["string_month"] = df["order_date"].apply(lambda x: x.strftime("%Y-%m"))
+            df = df.groupby(["month", "string_month", "member_number"]).nunique().reset_index()[["month", "string_month", "member_number", "orderId"]]            
+            df = df.groupby(["month", "string_month"])[["member_number", "orderId"]].count().reset_index()
+            df = df.sort_values("month")
+            df = df.sort_values("month")
+            
+            x = df["string_month"]
+            y = df["member_number"]
+                        
+            plt.figure(figsize=(12, 6))
+            plt.plot(x, y, linestyle='-')
+            plt.title("Số lượng khách mua hàng trong tháng")
+            plt.xlabel("Năm - Tháng")
+            plt.ylabel("Số lượng khách mua hàng")
+            plt.xticks(rotation = 45)
+            # Show the plot
+            st.pyplot(plt)
+            st.write("""<div><b>Theo xu hướng số lượng khách hàng tới cửa hàng giảm, 
+                    cũng tương đương số lượng đơn hàng giảm</b></div>""", unsafe_allow_html=True)
+            
+            #-------------------------------------------------------------------------------------
+            st.write("""<div style="font-size:1.3em; font-weight:bold">2. Tần suất mua hàng của khách hàng</div>""", unsafe_allow_html=True)
+            
+            x = myData.RFM["member_number"]
+            y = myData.RFM["Recency"]
+            plt.figure(figsize=(12, 6))      
+            plt.scatter(x, y, alpha=0.5)
+            plt.tick_params(labelbottom = False, bottom = False) 
+            st.pyplot(plt)
+            st.write("")
+
+            plt.figure(figsize=(12, 6))
+            plt.boxplot(y)
+            st.pyplot(plt)
+
+            st.write("""<div><b>Tần suất mua hàng của khách hàng tập trung từ 100 ngày đến 200 ngày</b></div>""", unsafe_allow_html=True)
+            st.write("")
+            
+            st.write("""<div><b>10 khách hàng có tần suất mua hàng cao nhất</b></div>""", unsafe_allow_html=True)
+            st.write(myData.RFM.sort_values("Recency").head(10))
+            
+            st.write("""<div><b>10 khách hàng có tần suất mua hàng thấp nhất</b></div>""", unsafe_allow_html=True)
+            st.write(myData.RFM.sort_values("Recency", ascending=False).head(10))
+            
+            st.write(f"""<div><b>Số lượng khách hàng có tần suất mua hàng 1 tháng (1 tháng quay lại mua hàng 1 lần): {myData.RFM[myData.RFM["Recency"] < 31]["member_number"].count()}</b></div>""", unsafe_allow_html=True)
+            st.write("")
+            st.write("Khách hàng thường xuyên của cửa hàng rất ít", unsafe_allow_html=True)
+            
+        elif (selected_menu.lower() == "sản phẩm"):
+            
+            text = " ".join(myData.Data["productName"])
+            wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+
+            plt.figure()
+            plt.imshow(wordcloud, interpolation="bilinear")
+            plt.axis("off")
+            st.pyplot(plt)
+                        
+            st.write(f"""<div>Dựa vào kết quả wordcloud những sản phẩm của hàng thường bán là:
+                     milk, rolls buns, root vegatables, fruit tropical, yogurt <br/>
+                     Tuy nhiên sẽ cần tìm hiểu thêm tần suất các sản phẩm được bán như thế nào?</div>""", unsafe_allow_html=True)            
+            st.write()
+            
+            df = pd.read_csv("output_data/product_r.csv")
+            x = df["productName"]
+            y = df["Recency"]
+            plt.figure(figsize=(12, 6))      
+            plt.scatter(x, y, alpha=0.5)
+            plt.tick_params(labelbottom = False, bottom = False) 
+            st.pyplot(plt)
+            st.write("")
+
+            plt.figure(figsize=(12, 6))
+            plt.boxplot(y)
+            st.pyplot(plt)
+            
+            plt.figure(figsize=(12, 6))
+            sns.distplot(y)            
+            st.pyplot(plt)
+            
+            st.write(f"""<div>Có nhiều sản phẩm được mua mỗi ngày.</div><br/>""", unsafe_allow_html=True)
+            text = " ".join(df[df["Recency"]<=2]["productName"])
+            wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+
+            plt.figure()
+            plt.imshow(wordcloud, interpolation="bilinear")
+            plt.axis("off")
+            st.pyplot(plt)
+            
+            st.write(f"""<div>Danh sách sản phẩm được mua với tần suất 7 ngày/ lần</div>""", unsafe_allow_html=True)                            
+            text = " ".join(df[df["Recency"]<=7]["productName"])
+            wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+
+            plt.figure()
+            plt.imshow(wordcloud, interpolation="bilinear")
+            plt.axis("off")
+            st.pyplot(plt)
+            
+            st.write(f"""<div>Danh sách sản phẩm được mua với tần suất 1 tháng / lần</div>""", unsafe_allow_html=True)
+                            
+            text = " ".join(df[df["Recency"]<=30]["productName"])
+            wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+
+            plt.figure()
+            plt.imshow(wordcloud, interpolation="bilinear")
+            plt.axis("off")
+            st.pyplot(plt)            
     
         
 #----------------------------------------------------------------------------------
