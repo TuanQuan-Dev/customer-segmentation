984365455fa9cd9a93c0d1c065e37624f12e550a
TuanQuan-Dev <tuanquan@gmail.com>
2024-06-22 09:08:36 +0700

    Update help.py

--- help.py
+++ help.py
@@ -7,47 +7,25 @@ def load_help():
          <ol>
             <li style="font-size:1.3em; font-weight:bold">Hướng dẫn sử dụng:
                 <ul>
-                    <li>Tạo ra các chiến dịch tiếp thị hiệu quả</li>
-                    <li>Giữ chân khách hàng: tạo chính sách đặc biệt cho khách hàng         
-                        quan trọng và thu hút lại khách hàng đã mua hàng trong quá khứ.</li>
-                    <li>
-                        Cải tiến dịch vụ: hiểu rõ nhu cầu khách hàng để điều chỉnh và tối
-                        ưu hóa dịch vụ, nâng cao sự hài lòng.
+                    <li>Khám phá dữ liệu: cung cấp cho chủ cửa hàng 1 góc nhìn về tình hình kinh doanh</li>
+                    <li>Kết quả: dựa vào nguồn dữ liệu cung cấp, hệ thống tiến hành phân nhóm khách hàng <br/>
+                        Hiển thị kết quả phân nhóm
                     </li>
                     <li>
-                        Tăng doanh thu: tập trung nguồn lực vào phân khúc khách hàng
-                        có lợi nhuận cao, dẫn đến tăng doanh thu và giảm chi phí bán hàng.
+                        Tra Cứu: tìm thông tin khách hàng
                     </li>
                 </ul>
             </li> 
-            <li style="font-size:1.3em; font-weight:bold">Thực hiện
+            <li style="font-size:1.3em; font-weight:bold">Cấu trúc ứng dụng
                 <ul>
-                Thu thập và phân tích dữ liệu mua hàng của khách hàng <br/>
-                Xác định các tiêu chí để phân nhóm khách hàng.                
+                    <li><b>homepage.py:</b> viết nội dung trang home</li>
+                    <li><b>help.py:</b> viết nội dung trang hướng dẫn</li>
+                    <li><b>data.py:</b> xử lý nguồn dữ liệu, phân nhóm khác hàng
+                        Dữ liệu sau khi phân nhóm được lưu thành file <i><b>customer_rfm.csv</b></i> trong thư mục output_data
+                    </li>
+                    <li><b>GUI.py:</b> file chính của giao diện, thiết kế giao diện của web, vẽ biểu đồ</li>
                 </ul>
-            </li>
-            </li> 
-            <li style="font-size:1.3em; font-weight:bold">Lợi ích<br/>                
-                <ul>
-                    <li>Tiếp thị tốt hơn
-                        <div>
-                            Khách hàng đã được phân nhóm cho phép cửa hàng đưa ra các chiến dịch tiếp thị quảng
-                            cáo tập trung hơn, đúng vào yêu cầu của khách hàng
-                        <div>
-                    </li>
-                    <li>Khả năng mở rộng
-                        <div>
-                            Cửa hàng hiểu rõ hơn về những sản phẩm mà khách hàng quan tâm. Từ đó mở rộng sản phẩm và dịch vụ 
-                            mới phù hợp với đối tượng mục tiêu
-                        </div>
-                    </li>
-                    <li>Giữ chân khách hàng
-                        <div>
-                            Xác định được khách hàng tiềm năng, giữ chân họ bằng những chương trình tiếp thị hiệu quả
-                        </div>
-                    </li>
-                </ul>
-            </li>
+            </li>                        
         </ol>
         </div> 
         """         
